000238dc45e014f9d07c35d44c8aa3ba9d1dc1fa
Son Nguyen <snguyen21@ssis.edu.vn>
2020-05-21 08:59:12 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import rotaryio
+import board
+import time
+ 
+encoder = rotaryio.IncrementalEncoder(board.D0, board.D1)
+last_position = None
+initialTime = time.monotonic()[
+TIC_PER_ROTATION = 256
+
+
+while True:
+    position = encoder.position 
+    currentTime = time.monotonic() - initialTime
+    print((currentTime,position))
+    
+    #if last_position is None or position != last_position:
+        #print(position)
+    last_position = position
